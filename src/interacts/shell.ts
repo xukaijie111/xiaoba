188d79888ba52ace00ea55f1610544789a90f289
xukaijie2022@126.com <xukaijie2022@126.com>
2022-08-16 18:31:51 +0800

    shell 交互

--- src/interacts/shell.ts
+++ src/interacts/shell.ts
@@ -3,20 +3,46 @@ import {
     Robot
 } from '../core/robot'
 
+const readline = require('readline')
+const Stream = require('stream')
+const cline = require('cline')
+const chalk = require('chalk')
+
+import {
+    Message
+} from '../core/message'
+
+let exitCommands = [
+    'exit',
+    'quit'
+]
 export class Shell {
+    cli:any
     options:Shell.options
     constructor(options:Shell.options) {
         this.options = options;
     }
 
 
-    send(){
+    send(message:Message){
+        let strings = message.getResponseText()
+        
 
+        Array.from(strings).forEach(str => console.log(chalk.bold(`${str}`)))
     }
 
 
     run() {
-        
+        let { robot } = this.options
+        this.cli = cline()
+
+        this.cli.command('*', input => {
+           if (exitCommands.includes(input)) return process.exit(0)
+           robot.receive(new Message({ text:input,interact:this }))
+        })
+    
+        this.cli.interact(`${robot.name}> `)
+      
     }
 }
 
@@ -28,4 +54,4 @@ export namespace Shell {
 }
 
 
-export const shell = (robot:Robot) => new Shell({robot})
+module.exports = (robot:Robot) => new Shell({robot})
