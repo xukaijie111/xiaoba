dcc6233d8b84128f94e0428832a8dabfec1f87b2
xukaijie2022@126.com <xukaijie2022@126.com>
2022-08-16 19:25:18 +0800

    账号密码隐藏

--- src/parsers/base.ts
+++ src/parsers/base.ts
@@ -17,16 +17,16 @@ class Base {
 
         robot.registerParser(/^火山账号/, (msg) => {
             new Response({ msg, robot })
-                .set(chalk.green(`账号:weixuhong`))
-                .set(chalk.green(`密码:S*0$&1ig!Wx6PtpFl3#^8I7U`))
-                .set(chalk.green(`网址:https://portal.shouqianba.com/portal/overview`))
+                // .set(chalk.green(`账号:weixuhong`))
+                // .set(chalk.green(`密码:S*0$&1ig!Wx6PtpFl3#^8I7U`))
+                // .set(chalk.green(`网址:https://portal.shouqianba.com/portal/overview`))
         })
 
 
         robot.registerParser(/^B端账(号|户)/i, (msg) => {
             new Response({ msg, robot })
-                .set(chalk.green(`小红帽便利店：21690003046807  13958099751  a111111`))
-                .set(chalk.green(`康一君测试商户：21690003074418    17705840828   a111111  115599`))
+                // .set(chalk.green(`小红帽便利店：21690003046807  13958099751  a111111`))
+                // .set(chalk.green(`康一君测试商户：21690003074418    17705840828   a111111  115599`))
         })
 
 
